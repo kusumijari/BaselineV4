1c168b6bceb19e45c48e6b1d0ff52a2815cf3d7e
kusumi <kusumi@INkusumi.local>
2015-09-10 11:05:41 +0530

    Added spec to check if the input contains the word book in it

---
+++ src/com/baseline/salestax/Parser.java
@@ -0,0 +1,15 @@
+//Receives input and parses it
+
+package com.baseline.salestax;
+
+public class Parser {
+    private String input;
+
+    public Parser(String input) {
+        this.input = input;
+    }
+
+    public boolean containsBook() {
+        return true;
+    }
+}
